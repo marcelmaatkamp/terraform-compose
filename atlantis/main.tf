3926435fbeb3bc158c06ed0fc166f12f29686235
Marcel Maatkamp <m.maatkamp@gmail.com>
2022-04-05 19:07:30 +0200

    local to var

--- atlantis/main.tf
+++ atlantis/main.tf
@@ -1,12 +1,5 @@
-locals { 
-
-  ngrok_name = "ngrok"
-  ngrok_port = 4040
-  ngrok_endpoint = "${local.ngrok_name}:${local.ngrok_port}"
-
-  atlantis_version = "v0.19.2"
-  atlantis_name = "atlantis"
-  atlantis_port = 4141
+locals {
+  ngrok_endpoint    = "${local.ngrok_name}:${local.ngrok_port}"
   atlantis_endpoint = "${local.atlantis_name}:${local.atlantis_port}"
 }
 
@@ -16,11 +9,11 @@ resource "docker_network" "atlantis" {
 
 resource "docker_container" "ngrok" {
   image    = "wernight/ngrok:latest"
-  name     = local.ngrok_name
-  hostname = local.ngrok_name
+  name     = var.ngrok_name
+  hostname = var.ngrok_name
   ports {
-    internal = local.ngrok_port
-    external = local.ngrok_port
+    internal = var.ngrok_port
+    external = var.ngrok_port
   }
   env = [
     "NGROK_PROTOCOL=http",
@@ -32,15 +25,15 @@ resource "docker_container" "ngrok" {
 }
 
 resource "docker_container" "atlantis" {
-  image    = "ghcr.io/runatlantis/atlantis:${local.atlantis_version}"
-  name     = local.atlantis_name
-  hostname = local.atlantis_name
+  image    = "ghcr.io/runatlantis/atlantis:${var.atlantis_version}"
+  name     = var.atlantis_name
+  hostname = var.atlantis_name
   depends_on = [
     docker_container.ngrok
   ]
   ports {
-    internal = local.atlantis_port
-    external = local.atlantis_port
+    internal = var.atlantis_port
+    external = var.atlantis_port
   }
   env = [
 
---
+++ atlantis/variables.tf
@@ -0,0 +1,18 @@
+variable "ngrok_name" {
+  default = "ngrok"
+}
+
+variable "" "ngrok_port" {
+  default = 4040
+}
+
+variable "atlantis_version" {
+  default = "v0.19.2"
+}
+variable "atlantis_name" {
+  default = "atlantis"
+}
+
+variable "atlantis_port" {
+  default = 4141
+}
